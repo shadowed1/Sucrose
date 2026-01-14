2d1e57b6743ddfdacd3b2c08b31c55642cd256ca
shadowed1 <itszachg@gmail.com>
2026-01-14 16:48:19 -0500

    Update and rename elevate_installer to elevate_installer.sh

--- elevate_installer.sh
+++ elevate_installer.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+# ChromeOS Elevated Shell
+# shadowed1
 # Must be run as sudo logged in as chronos in VT-2
+
 mkdir -p /usr/local/bin
 curl -L https://raw.githubusercontent.com/shadowed1/elevate/main/elevate.sh -o /usr/local/bin/elevate
 curl -L https://raw.githubusercontent.com/shadowed1/elevate/main/elevate-daemon.sh -o /usr/local/bin/elevate-daemon
